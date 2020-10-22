c0682c760cdc663fe2dbcdb21549e46e21b13724
miracle <anajemiracle@gmail.com>
2020-10-22 21:06:14 +0100

    first-commit

--- models/Products.js
+++ models/Products.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const Productschema = new mongoose.Schema({
-  product: {
-    type: String,
-    required: [true, "Please this field is required"],
-    trim: true,
-  },
-  serial: {
-    type: Number,
-    required: [true, "please this field is required"],
-    trim: true,
-    unique: true,
-  },
-});
+// const Productschema = new mongoose.Schema({
+//   product: {
+//     type: String,
+//     required: [true, "Please this field is required"],
+//     trim: true,
+//   },
+//   serial: {
+//     type: Number,
+//     required: [true, "please this field is required"],
+//     trim: true,
+//     unique: true,
+//   },
+// });
 
-module.exports = mongoose.model("Product", Productschema)
+// module.exports = mongoose.model("Product", Productschema)
